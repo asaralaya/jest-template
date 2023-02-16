03e7d79046b71d6fc892e26e5f17721639cd554e
Abhishek P N <abhishekpn98@gmail.com>
2023-02-16 17:56:09 +0530

    Code changes to replace imports and class name

--- src/templateBuilder.ts
+++ src/templateBuilder.ts
@@ -1,103 +1,78 @@
-
 const fs = require('fs');
 var path = require('path');
-const fsPromises = fs.promises;
-
 
 //reading path and filaname pattern from command line
 const userDirectory = process.argv.at(2);
 const fileName = process.argv.at(3);
-var importsArray = [];
+var importsArray = []
 var className
 
+//method to create spec files
+var createSpecfile = function (fileName, importsArray, className) {
+    // files.forEach(fileName => {
+    let indexPosition = fileName.lastIndexOf('.')
+    fileName = fileName.split('');
+    // Insert the string at the index position
+    fileName.splice(indexPosition, 0, '.spec')
+    let specFilePath = fileName.join('')
+    if (!fs.existsSync(specFilePath)) {
+        //copying the template
+        fs.copyFileSync("./src/template.txt", specFilePath);
+        fs.readFile(specFilePath, 'utf8', function (err, data) {
+            var formatted = data.replace(/{{imports}}/g, importsArray).replace(/{{ClassName}}/g, className);
+            fs.writeFile(specFilePath, formatted, 'utf8', function (err) {
+                if (err) return console.log(err);
+                console.log("Spec file created:", specFilePath)
+            });
+        });
+    }
+    else { console.log("Spec file already exists for:", specFilePath) }
+    //   });
+}
 
-//method to search for the file in the directory
-var searchRecursive = function (dir, pattern) {
-
-    // This is where we store pattern matches of all files inside the directory
+//method to search for files
+var searchRecursive = function (startPath, filter) {
     var results = [];
 
-    // Read contents of directory
-    fs.readdirSync(dir).forEach(function (dirInner) {
-        if (dirInner.includes('node_modules') || dirInner.includes('.git') || dirInner.includes('coverage'))
-            return;
-        // Obtain absolute path
-        dirInner = path.resolve(dir, dirInner);
+    if (!fs.existsSync(startPath)) {
+        console.log("No dir ", startPath);
+        return;
+    }
 
-        // Get stats to determine if path is a directory or a file
-        var stat = fs.statSync(dirInner);
-
-        // If path is a directory, scan it and combine results
+    var files = fs.readdirSync(startPath);
+    for (var i = 0; i < files.length; i++) {
+        var filename = path.join(startPath, files[i]);
+        if (filename.includes('node_modules') || filename.includes('.git') || filename.includes('coverage'))
+            continue;
+        var stat = fs.lstatSync(filename);
         if (stat.isDirectory()) {
-            results = results.concat(searchRecursive(dirInner, pattern));
+            results = results.concat(searchRecursive(filename, filter)); //recurse
         }
-
-        // If path is a file and ends with pattern then push it onto results
-        if (stat.isFile() && dirInner.match(pattern)) {
-            results.push(dirInner);
+        else if (filename.indexOf(filter) >= 0) {
+            results.push(filename);
         }
-    });
+    }
     return results;
+}
+
+//reading original file
+async function readOriginalfile() {
+    for (let fileName of files) {
+        importsArray = []
+        const data = await fs.promises.readFile(fileName, 'utf-8');
+        const contents = data.toString()
+        for (const [str,] of contents.matchAll(/import([ \n\t]*(?:[^ \n\t\{\}]+[ \n\t]*,?)?(?:[ \n\t]*\{(?:[ \n\t]*[^ \n\t"'\{\}]+[ \n\t]*,?)+\})?[ \n\t]*)from[ \n\t]*(['"])([^'"\n]+)(?:['"])/g)) {
+            importsArray.push(str.replaceAll('\n', ''))
+        }
+        for (const [str,] of contents.matchAll(/export class [A-Z][A-Za-z]*/g)) {
+            let temp = str.split(" ")
+            className = temp[2]
+        }
+        createSpecfile(fileName, importsArray, className);
+    }
 };
 
-//method to create spec files
-var createSpecfile = function () {
-    files.forEach(fileName => {
-        let indexPosition = fileName.lastIndexOf('.')
-        fileName = fileName.split('');
-        // Insert the string at the index position
-        fileName.splice(indexPosition, 0, '.spec')
-        let specFilePath = fileName.join('')
-        fs.copyFileSync("./src/template.txt", specFilePath);
-    });
-}
-
-var readOriginalfile = function () {
-    files.forEach(fileName => {
-        //reading original file
-        fs.readFile(fileName, 'utf-8', (error, data) => {
-            if (error) throw error;
-            var array = data.toString().split("\n");
-            for (let i in array) {
-                if (array[i].startsWith('import')) {
-                    importsArray.push(array[i])
-                }
-                else if (array[i].includes('export class')) {
-                    let temp = array[i].split(" ")//now you have 3 words in temp
-                    className = temp[2]
-                }
-            }
-            console.log(importsArray)
-        });
-
-
-        // const origin = fs.createReadStream(fileName, {
-        //     flags: 'r',
-        //     // read data as a string not as a buffer
-        //     encoding: 'utf8'
-        // });
-        // const transform = new stream.Transform({
-        //     // accept data as a strings
-        //     writableObjectMode: true,
-        //     transform: function removeNewLines(chunk, encoding, callback){
-        //         callback(null, chunk.replace(/\n/g, ''));
-        //     }
-        // });
-        // const destination = fs.createWriteStream('input.txt', {
-        //     flags: 'w+',
-        //     // write data as a strings, this is default value
-        //     encoding: 'utf8'
-        // });
-
-        // origin.pipe(transform).pipe(destination);
-
-    });
-
-
-}
-
 var files = searchRecursive(userDirectory, fileName);
-createSpecfile();
 readOriginalfile();
 
 
